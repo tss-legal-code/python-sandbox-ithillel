3583e5affbfd6f13340bb4f390fc7ccb3c1313a6
tss-legal-code <tss.legal.code@github.com>
2019-05-28 21:45:35 +0300

    corrected version with printable task

--- hometask_04/04-01.py
+++ hometask_04/04-01.py
@@ -1,12 +1,23 @@
+task = """
+1. Дан массив. Реализовать функцию которая будет 
+переставлять 2 выбранных элемента списка местами. 
+
+Функция должна иметь вид:
+def swap(target_list, item_index1, item_index2).
+
+"""
+
 def swap (target_list, item_index1,item_index2):
     print("old target_list is: {}".format(target_list))
     print("swapping {} with {}".format(item_index1,item_index2))
     target_list[item_index1], target_list[item_index2] = target_list[item_index2], target_list[item_index1]
     print("new target_list is: {}".format(target_list))
     print()
-l = [x for x in range(40)]
+l = [x for x in range(20)]
 
-swap(l, 10,20)
+print(task)
 
-swap(l, 20,39)
+swap(l, 0,19)
+
+swap(l, 1,18)
 
