a6b05368e1565517063447a034c75daaa404a3b3
tss-legal-code <tss.legal.code@github.com>
2019-05-29 21:29:42 +0300

    nicely formatted as hometask some code, written at lesson

--- hometask_05/05_01.py
+++ hometask_05/05_01.py
@@ -1,144 +1,137 @@
+task = """
+1. Реализовать декораторы рассмотренные на лекции
+
+1.1. таймер - выводит время работы функции
+1.2. запоминатель - кэширует аргументы функции и её результаты, и возвращает готовые значения при повторном обращении
+1.3. счетчик вызовов - считает сколько раз вызывалась функция
+1.4. единичный вызов - выполняет вызов функции только при первом обращении, затем выводит сообщение, что функция уже вызывалась
+
+"""
+
+
+print("{:=^80s}".format("< 1.1. @timer >"))
+
 import time
 
+def timer(function):
+    def wrapper(x):
+        print("Starting execution of \'{}\'".format(function.__name__))
+        t0 = time.time()
+        function(x)
+        t1 = time.time()
+        print("Execution of \'{}\' took \'{:.10f}\' seconds".format(function.__name__, t1-t0))
+    return wrapper
 
-#timer
-# def dec(fun):
-#     def wrapper(x):
-#         t0 = time.time()
-#         print("decorator")
-#         fun(x)
-#         print("/decorator")
-#         t1 = time.time()
-#         print("elapsed {}".format(t1-t0))
-#     return wrapper
-#
-# @dec
-# def funct(x):
-#     print(x)
-#     for y in range(1000000):
-#         d = y ** 3
-#
-# funct(10)
+@timer
+def function_0(x):
+    for y in range(1000000):
+        d = y ** 3
+    print("... function processed \'{}\'".format(x))
 
-#запоминатель
+function_0("some argument")
 
-# def dec1(fun):
-#     d={}
-#     def wrapper(args):
-#         if args in d:
-#             t0 = time.time()
-#             print(d[args])
-#             t1 = time.time()
-#             print("elapsed {}".format(t1 - t0))
-#         else:
-#             t0 = time.time()
-#             #print("decorator")
-#             res = fun(args)
-#             print(res)
-#             #print("/decorator")
-#             t1 = time.time()
-#             print("elapsed {}".format(t1-t0))
-#             d[args]=res
-#             print("args and results:", d)
-#     return wrapper
-#
-# @dec1
-# def funct1(x):
-#     print(x)
-#     for y in range(1000000):
-#         d = y ** 3
-#     return x*23
-#
-# funct1(10)
-#
-# funct1(12)
-#
-# funct1(14)
-#
-# funct1(12)
 
-#счетчик вызовов
 
-# def dec2(fun):
-#     d={}
-#     f=0
-#     def wrapper(args):
-#         nonlocal f
-#
-#         if args in d:
-#             t0 = time.time()
-#             print(d[args])
-#             t1 = time.time()
-#             print("elapsed {}".format(t1 - t0))
-#         else:
-#             t0 = time.time()
-#             #print("decorator")
-#             res = fun(args)
-#             print(res)
-#             #print("/decorator")
-#             t1 = time.time()
-#             print("elapsed {}".format(t1-t0))
-#             d[args]=res
-#             print("args and results:", d)
-#         f += 1
-#         print("call No.{}".format(f))
-#     return wrapper
-#
-# @dec2
-# def funct2(x):
-#     print(x)
-#     for y in range(1000000):
-#         d = y ** 3
-#     return x*23
-#
-# funct2(10)
-#
-# funct2(12)
-#
-# funct2(14)
-#
-# funct2(12)
 
-# единичный вызов
-#
-# def dec3(fun):
-#     d={}
-#     f=0
-#     def wrapper(args):
-#         nonlocal f
-#         if f == 0:
-#             if args in d:
-#                 t0 = time.time()
-#                 print(d[args])
-#                 t1 = time.time()
-#                 print("elapsed {}".format(t1 - t0))
-#             else:
-#                 t0 = time.time()
-#                 # print("decorator")
-#                 res = fun(args)
-#                 print(res)
-#                 # print("/decorator")
-#                 t1 = time.time()
-#                 print("elapsed {}".format(t1 - t0))
-#                 d[args] = res
-#                 print("args and results:", d)
-#             f += 1
-#             print("call No.{}".format(f))
-#         else:
-#             print("Do not call me no more, bastard! I mean it!")
-#     return wrapper
-#
-# @dec3
-# def funct3(x):
-#     print(x)
-#     for y in range(1000000):
-#         d = y ** 3
-#     return x*23
-#
-# funct3(10)
-#
-# funct3(12)
-#
-# funct3(14)
-#
-# funct3(12)
+print("{:=^80s}".format("< 1.2. @rememberer >"))
+#добавил обработку произвольного числа позиционных аргументов
+
+def rememberer(function):
+    d={}
+    def wrapper(*args):
+        if args in d:
+            print("\nFunction \'{}\' with arguments \'{}\' was recently calculated, so result was stored and now it is extracted from dictionary".format(function.__name__, args))
+            print("result of calculation", d[args])
+        else:
+            print("\nFirst run of function \'{}\' with arguments \'{}\'".format(function.__name__, args))
+            res = function(*args)
+            print("result of calculation", res)
+            d[args]=res
+    return wrapper
+
+@rememberer
+def function_1(t, x):
+    print(x)
+    for y in range(1000000):
+        d = y ** 3
+    return x*23
+
+function_1("some argument", 12)
+
+function_1("some argument", 12) #check it
+
+function_1("some argument", 13)
+
+function_1("some argument", 13) #check it
+
+
+
+print("{:=^80s}".format("< 1.3. @counter >"))
+# добавил запоминание количества вызовов КАЖДОЙ функции с декоратором
+
+def counter(function):
+    d={}
+    def wrapper(*args):
+        if function.__name__ not in d:
+            d[function.__name__] = 1
+            print("Function {} was called for the first time!".format(function.__name__))
+            res = function(*args)
+            print("result:", res)
+        else:
+            d[function.__name__] += 1
+            print("Function {} was called {} times!".format(function.__name__, d[function.__name__]))
+            res = function(*args)
+            print("result:", res)
+    return wrapper
+
+@counter
+def function_2_1(t, x):
+    for y in range(1000000):
+        d = y ** 3
+    return x*23
+
+@counter
+def function_2_2(t, x):
+    for y in range(1000000):
+        d = y ** 3
+    return x*23
+
+function_2_1("some argument", 12)
+
+function_2_1("some argument", 12) #check it
+
+function_2_2("some argument", 13)
+
+function_2_2("some argument", 13) #check it
+
+print("{:=^80s}".format("< 1.4. @single_call >"))
+# логическая проверка элегантно работает для ЕДИНИЧНОГО вызова,
+# а для фиксированного количества разрешенных вызовов можно реализовать декоратор
+# так же как и счетчик, только с условием прекращения выполнения на определенной итерации
+
+def single(function):
+    first_run = True
+    def wrapper(*args):
+        nonlocal first_run
+        if first_run:
+            print("first run of \'{}\'".format(function.__name__))
+            res = function(*args)
+            print("result:", res)
+        else:
+            print("Error! \'{0}\' has already been run! NOTE: \'{0}\' can run only once!".format(function.__name__))
+        first_run = False
+    return wrapper
+
+@single
+def function_3(t, x):
+    for y in range(1000000):
+        d = y ** 3
+    return x*23
+
+function_3("some argument", 12)
+
+function_3("some argument", 12) #check it
+
+function_3("some argument", 13) #check it
+
+function_3("some argument", 13) #check it
