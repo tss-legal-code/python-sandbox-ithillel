0e973a328ebc738fee37681e85df9e763341dc50
tss-legal-code <tss.legal.code@github.com>
2019-05-30 21:00:58 +0300

    practice strings

---
+++ lesson_06/lesson_06.py
@@ -0,0 +1,15 @@
+task = """
+Строки. Файлы
+"""
+print(task)
+print("{:=^80s}".format("< Строки. Файлы >"))
+
+print(r"\t\t\n") #'raw' string
+
+
+s = "some text"
+
+print("asfag : %s" % [1,2,3])
+
+print("asfag : %s" % "asd")
+print(b'\42')
