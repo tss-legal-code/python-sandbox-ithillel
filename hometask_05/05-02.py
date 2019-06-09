d246ade2196eff93160edc168409ffe8f0ad8187
tss-legal-code <tss.legal.code@github.com>
2019-06-09 19:37:44 +0300

    solved hometask 5

--- hometask_05/05-02.py
+++ hometask_05/05-02.py
@@ -5,3 +5,63 @@ task = """
 """
 
 print(task)
+
+from functools import singledispatch
+from random import randint
+
+print("{:=^80s}".format("< 1. @singledispatch >"))
+
+@singledispatch
+def get_random(r=0, verbose=False):
+    tmp = randint(0,r)
+    if r:
+        print("Generating random integer from zero to argument if argument is integer (otherwise get something else ^_^)")
+        print("Random integer: ", tmp)
+    return tmp
+
+
+@get_random.register(str)
+def _(r, verbose=False):
+    tmp = randint(0, len(r)-1)
+    if verbose:
+        print("You passed string \'{}\'! Get it\'s random char!".format(r))
+        print("Random char position: ", tmp)
+    return r[tmp]
+
+
+@get_random.register(list)
+def _(r, verbose=False):
+    tmp = randint(0, len(r)-1)
+    if verbose:
+        print("You passed list \'{}\'! Get it\'s random element!".format(r))
+        print("Random list index: ", tmp)
+    return r[tmp]
+
+i = get_random(100, verbose = True)
+print("i: \'{}\'".format(i))
+s = get_random("The following documentation is automatically generated from the Python source files.", verbose = True)
+print("s: \'{}\'".format(s))
+l = get_random("The following documentation is automatically generated from the Python source files.".split(), verbose = True)
+print("l: \'{}\'".format(l))
+
+print("{:=^80s}".format("< 1. @wraps >"))
+
+from functools import wraps
+
+
+def decorator(func):
+    @wraps(func)
+    def wrapper(*args):
+        print("Before call: wrapped function \'{}\' was called".format(func.__name__))
+        return func(*args)
+    return wrapper
+
+@decorator
+def documented_function(x):
+    """DOCSTRING: This function created to demonstate @wraps"""
+    return "You entered {}".format(x)
+
+
+print("name: ", documented_function.__name__)
+print("docstring: ", documented_function.__doc__)
+print("calling: ", documented_function("Some args"))
--- hometask_05/05-03.py
+++ hometask_05/05-03.py
@@ -3,18 +3,91 @@ task = """
 3. Сделать свою реализацию декоратора lru_cache, 
 аналогичную библиотечному (описание в конце презентации).
 
+LRU (least recently used) — это алгоритм, при котором вытесняются значения, которые 
+дольше всего не запрашивались. Соответственно, необходимо хранить время последнего 
+запроса к значению. И как только число закэшированных значений превосходит N необходимо 
+вытеснить из кеша значение, которое дольше всего не запрашивалось.
+
+Задача - 1
+Создать декоратор lru_cache (подобный тому который реализован в Python).
+
+Задача - 2
+Ваш lru_cache декоратор должен иметь служебный метод cache_info  - статистика 
+использования вашего кеша (например: сколько раз вычислялась ваша функция, а 
+сколько раз значение было взято из кеша, сколько места свободно в кэше, время жизни кэша)
+
+Задача - 3
+Ваш lru_cache декоратор должен иметь служебный метод cache_clear - очищает кэш
+
 """
 
 print(task)
 
-from functools import lru_cache
+print("{:=^80s}".format("< 1-3. @personal_lru_cache >"))
+from time import time, sleep
 
 
-#@lru_cache(maxsize=None)
+def personal_lru_cache(func, max_size=5, ttl=1):
+    cache = {}
+    call_counter = 0
+    taken_from_cache_counter = 0
+
+    def wrapper(*args, **kwargs):
+        nonlocal call_counter, taken_from_cache_counter
+        call_counter += 1
+
+        key = str([str(a) for a in args] + [str(k) for k in kwargs])
+
+        if (key in cache) and (time() - cache[key]["created"] <= ttl):
+            print("Using a cached value")
+            cache[key]["used"] = time()
+            taken_from_cache_counter += 1
+
+        else:
+            if len(cache) == max_size:
+                last_used = sorted(cache.keys(), key=lambda x: cache[x]["used"])[0]
+                del cache[last_used]
+                print("Deleted a most long ago used key \'{}\' from cache".format(last_used))
+
+            cache[key] = {}
+            cache[key]["created"] = cache[key]["used"] = time()
+            print("Added/Re-added a key \'{}\' to a cache".format(key))
+            cache[key]["value"] = func(*args, **kwargs)
+
+        def cache_info():
+            if call_counter: print("{:=^80s}".format("< 2. Cache info report>"))
+            print("Function {} was called {} times".format(func.__name__, call_counter))
+            print("Value was taken from cache {} times".format(taken_from_cache_counter))
+            print("TTL of values in cache is {} sec".format(ttl))
+            print("Cache free space is {} items".format(max_size - len(cache)))
+            if call_counter:
+                print("Cache contents are:")
+                for x in sorted(cache.keys()):
+                    print(x, cache[x])
+
+        def cache_clear():
+            nonlocal call_counter, taken_from_cache_counter
+            print("{:=^80s}".format("< 3. Cache clear report>"))
+            cache.clear()
+            call_counter = taken_from_cache_counter = 0
+            cache_info()
+
+        wrapper.cache_info = cache_info
+        wrapper.cache_clear = cache_clear
+
+        return cache[key]["value"]
+
+    return wrapper
+
+
+@personal_lru_cache
 def fib(n):
     if n <= 2:
         return n
     return fib(n - 1) + fib(n - 2)
 
-print([fib(i) for i in range(200)])
-print(fib.cache_info())
+
+calc = [fib(i) for i in range(2000)]
+print(calc)
+fib.cache_info()
+fib.cache_clear()
